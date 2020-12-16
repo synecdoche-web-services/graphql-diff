f49be99ea6f2090aea667e24b449277c0be2829b
Evan Rusmisel <evanrusmisel@gmail.com>
2020-12-15 19:03:36 -0600

    Start setting up lib w/rollup, ava, prettier...

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+
+export default {
+    input: "src/index.ts",
+    plugins: [
+        resolve(),
+        typescript({
+            declaration: true,
+            cacheDir: '.rollup.cache',
+            outDir: 'dist',
+            tsBuildInfoFile: 'dist/buildinfo.tscache',
+        }),
+        terser(),
+    ],
+    output: {
+        dir: "dist",
+        format: "umd",
+        name: "GraphQLDiff",
+    },
+}
